00aec188753b59045e543cecb5c64409ae032143
Xiangzhuang Shen <datahonor@gmail.com>
2024-02-03 14:07:24 +0800

    counter init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 pub mod guess;
 pub mod log;
 pub mod generic_func;
+pub mod counter;
 
 // use crate::guess::guess_game;
 // use crate::log::run_logger_example;
-use crate::generic_func::run_generic_example;
+// use crate::generic_func::run_generic_example;
+use crate::counter::run_counter_example;
 
 fn main() {
     // guess_game();
     // run_logger_example();
-    run_generic_example();
+    // run_generic_example();
+    run_counter_example();
 }
 
